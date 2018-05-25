8cc1622d17ae10620c50fc430bd002e33e51dc50
Arnaud Roger <arnaud.roger@gmail.com>
2018-05-25 10:39:47 +0100

    more bench

--- 05_arraylistvsarray/src/main/java/com/github/arnaudroger/Seq2ArrayAccess.java
+++ 05_arraylistvsarray/src/main/java/com/github/arnaudroger/Seq2ArrayAccess.java
@@ -32,18 +32,14 @@
 package com.github.arnaudroger;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.CompilerControl;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.Random;
-
 @State(Scope.Benchmark)
-public class SeqArrayAccess {
+public class Seq2ArrayAccess {
 
     @Param({"10", "1000", "1000000"})
     int size;
@@ -56,10 +52,12 @@ public class SeqArrayAccess {
     }
     
     @Benchmark
-    public void testGet(Blackhole b) {
+    public long testGet() {
+        long total = 0;
         for(Long l : data) {
-            b.consume(l);
+            total += l;
         }
+        return total;
     }
 
 }
--- 05_arraylistvsarray/src/main/java/com/github/arnaudroger/Seq2ListAccess.java
+++ 05_arraylistvsarray/src/main/java/com/github/arnaudroger/Seq2ListAccess.java
@@ -32,34 +32,36 @@
 package com.github.arnaudroger;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.CompilerControl;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 @State(Scope.Benchmark)
-public class SeqArrayAccess {
+public class Seq2ListAccess {
 
     @Param({"10", "1000", "1000000"})
     int size;
     
-    Long[] data;
+    ArrayList<Long> data;
     
     @Setup
     public void setUp() {
-        data = Data.dataArray(size);
+        data = Data.dataArrayList(size);
     }
     
-    @Benchmark
-    public void testGet(Blackhole b) {
-        for(Long l : data) {
-            b.consume(l);
-        }
-    }
 
+    @Benchmark
+    public long testIndexed() {
+        ArrayList<Long> data = this.data;
+        int size = data.size();
+        long total = 0;
+        for(int i = 0; i < size; i++) {
+            total += data.get(i);
+        }
+        return total;
+    }
 }
